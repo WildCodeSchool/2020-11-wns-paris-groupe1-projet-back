79c67398ffb91bbc7e36b6db15f3f372a49ba129
christopheLem <lemercier60@gmail.com>
2021-06-03 15:52:49 +0200

    feat(security): add security package

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import rateLimit from 'express-rate-limit';
+import hpp from 'hpp';
+
 import mongoConnection from './config/mongoConnection.js';
 import { getApolloServer } from './server.js';
 
@@ -14,6 +20,25 @@ const main = async () => {
 
     // Express server
     const app = express();
+    // Permet CORS
+    app.use(cors());
+    // Configure en-têtes HTTP
+    app.use(
+      helmet({
+        contentSecurityPolicy: false,
+      })
+    );
+    // Empêche les attaques cross-site scripting (xss)
+    app.use(xss());
+    // Limite le nombre de requête
+    const limiter = rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 mins
+      max: 100,
+    });
+    app.use(limiter);
+    // Empêche la pollution des paramètres http
+    app.use(hpp());
+
     const server = await getApolloServer();
 
     server.applyMiddleware({ app, path: '/graphql' });
